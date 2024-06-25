d1fee3221e0c11fa312d44334c3d984d944a5687
Chadi hoayeK <chadihoayek@Chadis-MacBook-Pro.local>
2024-06-25 16:30:09 -0700

    added routes

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const { connect, connection } = require('mongoose');
+
+connect('mongodb://localhost/developersApplications', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = connection;
+
+
