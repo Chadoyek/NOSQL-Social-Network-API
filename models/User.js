fe8a8b166a21f94b197c9db8d5e1deaf9d8d7cfa
Chadi hoayeK <chadihoayek@Chadis-MacBook-Pro.local>
2024-06-25 17:35:06 -0700

    created models

--- models/User.js
+++ models/User.js
@@ -1,3 +1,48 @@
-const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+
+const userSchema = new Schema({
+        username: {
+            type: String,
+            unique: true,
+            trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address'],
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+},
+//overriding  default behavior
+    {
+    toJSON: {
+        virtuals:true
+    },
+    id:false,
+    }
+);
+
+//retrieves the length of the user's `friends` array field on query.
+userSchema.virtual(`friendCount`)
+            .get(function() {
+                return this.friends.length;
+            })
+
+const User = model('User', userSchema);
 
 module.exports = User;
+
+
